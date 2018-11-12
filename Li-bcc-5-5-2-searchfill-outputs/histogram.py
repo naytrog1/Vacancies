7d4e41cb91608529079cbe068341771b887a75ab
naytrog1 <naytrog1@gmail.com>
2018-11-11 21:21:38 -0300

    done

--- Li-bcc-5-5-2-searchfill-outputs/histogram.py
+++ Li-bcc-5-5-2-searchfill-outputs/histogram.py
@@ -3,5 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 a = np.loadtxt('Output_vacancies_Li-bcc-5-5-2.txt',usecols=5)
 plt.hist(a,100)
-z
+plt.xlabel("Vacancias Encontradas")
+plt.ylabel("Cantidad de Archivos")
 plt.show()
